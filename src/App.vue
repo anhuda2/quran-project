31c0dd9fdeabf57354a53007185a5f2b759c4fc9
anhuda2 <115517050+anhuda2@users.noreply.github.com>
2023-03-27 11:40:26 +0700

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <QuranLayout>
+      <RealTimeReading />
+      <div class="quran-container">
+        <QuranicVerse />
+        <Bookmark :title="'My Bookmarks'" :bookmarks="bookmarks" @addBookmark="addBookmark" @removeBookmark="removeBookmark" />
+      </div>
+    </QuranLayout>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuranicVerse from '@/components/QuranicVerse.vue'
+import QuranLayout from '@/components/QuranLayout.vue'
+import RealTimeReading from '@/components/RealTimeReading.vue'
+import Bookmark from '@/components/Bookmark.vue'
+import firebase from '@/firebase.js'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuranicVerse,
+    QuranLayout,
+    RealTimeReading,
+    Bookmark
+  },
+  data() {
+    return {
+      bookmarks: []
+    }
+  },
+  methods: {
+    addBookmark(bookmark) {
+      this.bookmarks.push(bookmark)
+    },
+    removeBookmark(id) {
+      const index = this.bookmarks.findIndex((bookmark) => bookmark.id === id)
+      if (index !== -1) {
+        this.bookmarks.splice(index, 1)
+      }
+    }
+  },
+  mounted() {
+    // Load bookmarks from Firebase Realtime Database
+    firebase
+      .database()
+      .ref('bookmarks')
+      .once('value')
+      .then((snapshot) => {
+        const bookmarksObject = snapshot.val()
+        if (bookmarksObject) {
+          const bookmarksArray = Object.keys(bookmarksObject).map((key) => ({
+            id: key,
+            surah: bookmarksObject[key].surah,
+            verse: bookmarksObject[key].verse,
+            text: bookmarksObject[key].text
+          }))
+          this.bookmarks = bookmarksArray
+        }
+      })
+
+    // Listen for new bookmarks added to Firebase Realtime Database
+    firebase
+      .database()
+      .ref('bookmarks')
+      .on('child_added', (snapshot) => {
+        const newBookmark = {
+          id: snapshot.key,
+          surah: snapshot.val().surah,
+          verse: snapshot.val().verse,
+          text: snapshot.val().text
+        }
+        this.addBookmark(newBookmark)
+      })
+
+    // Listen for bookmarks removed from Firebase Realtime Database
+    firebase
+      .database()
+      .ref('bookmarks')
+      .on('child_removed', (snapshot) => {
+        this.removeBookmark(snapshot.key)
+      })
   }
 }
 </script>
@@ -21,6 +92,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
